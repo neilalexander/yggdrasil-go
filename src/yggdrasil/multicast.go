756d366da099e435d2805aa5052da0c7456082ea
Neil Alexander <neilalexander@users.noreply.github.com>
2019-03-03 23:44:35 +0000

    Some work on multicast

--- src/yggdrasil/multicast.go
+++ src/yggdrasil/multicast.go
@@ -8,16 +8,21 @@ import (
 	"sync"
 	"time"
 
+	"golang.org/x/net/dns/dnsmessage"
 	"golang.org/x/net/ipv6"
 )
 
 type multicast struct {
-	core        *Core
-	reconfigure chan chan error
-	sock        *ipv6.PacketConn
-	groupAddr   string
-	myAddr      *net.TCPAddr
-	myAddrMutex sync.RWMutex
+	core         *Core
+	reconfigure  chan chan error
+	sock         *ipv6.PacketConn
+	dnsSock      *ipv6.PacketConn
+	groupAddr    string
+	dnsGroupAddr string
+	myAddr       *net.TCPAddr
+	myAddrMutex  sync.RWMutex
+	zones        []string
+	zonesMutex   sync.RWMutex
 }
 
 func (m *multicast) init(core *Core) {
@@ -29,21 +34,164 @@ func (m *multicast) init(core *Core) {
 			m.myAddrMutex.Lock()
 			m.myAddr = m.core.tcp.getAddr()
 			m.myAddrMutex.Unlock()
+			m.zonesMutex.Lock()
+			m.zones = []string{}
+			for _, intf := range m.interfaces() {
+				m.zones = append(m.zones, intf.Name)
+			}
+			m.zonesMutex.Unlock()
 			e <- nil
 		}
 	}()
 	m.groupAddr = "[ff02::114]:9001"
+	m.dnsGroupAddr = "[ff02::fb]:5353"
+	m.zonesMutex.Lock()
+	m.zones = []string{}
+	for _, intf := range m.interfaces() {
+		m.zones = append(m.zones, intf.Name)
+	}
+	m.zonesMutex.Unlock()
 	// Check if we've been given any expressions
-	if count := len(m.interfaces()); count != 0 {
+	if count := len(m.zones); count != 0 {
 		m.core.log.Infoln("Found", count, "multicast interface(s)")
 	}
 }
 
 func (m *multicast) start() error {
+	if err := m.startLegacy(); err != nil {
+		return err
+	}
+	if err := m.startMulticastDNS(); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (m *multicast) startMulticastDNS() error {
 	if len(m.interfaces()) == 0 {
-		m.core.log.Infoln("Multicast discovery is disabled")
+		m.core.log.Infoln("Multicast DNS discovery is disabled")
 	} else {
-		m.core.log.Infoln("Multicast discovery is enabled")
+		m.core.log.Infoln("Multicast DNS discovery is enabled")
+		addr, err := net.ResolveUDPAddr("udp", m.dnsGroupAddr)
+		if err != nil {
+			return err
+		}
+		listenString := fmt.Sprintf("[::]:%v", addr.Port)
+		lc := net.ListenConfig{
+			Control: m.multicastReuse,
+		}
+		conn, err := lc.ListenPacket(context.Background(), "udp6", listenString)
+		if err != nil {
+			return err
+		}
+		m.dnsSock = ipv6.NewPacketConn(conn)
+		if err = m.dnsSock.SetControlMessage(ipv6.FlagDst, true); err != nil {
+			// Windows can't set this flag, so we need to handle it in other ways
+		}
+
+		// TODO: uncomment the below when we no longer have the legacy listener
+		// as I don't really think we need to call this twice
+		//m.multicastWake()
+		go m.listenMulticastDNS()
+		go m.announceMulticastDNS()
+	}
+	return nil
+}
+
+func (m *multicast) announceMulticastDNS() {
+	/*var anAddr net.TCPAddr
+	m.myAddrMutex.Lock()
+	m.myAddr = m.core.tcp.getAddr()
+	m.myAddrMutex.Unlock()
+	groupAddr, err := net.ResolveUDPAddr("udp6", m.dnsGroupAddr)
+	if err != nil {
+		panic(err)
+	}
+	destAddr, err := net.ResolveUDPAddr("udp6", m.dnsGroupAddr)
+	if err != nil {
+		panic(err)
+	}
+	for {
+		for _, iface := range m.interfaces() {
+			m.sock.JoinGroup(&iface, groupAddr)
+			addrs, err := iface.Addrs()
+			if err != nil {
+				panic(err)
+			}
+			m.myAddrMutex.RLock()
+			anAddr.Port = m.myAddr.Port
+			m.myAddrMutex.RUnlock()
+			for _, addr := range addrs {
+				addrIP, _, _ := net.ParseCIDR(addr.String())
+				if addrIP.To4() != nil {
+					continue
+				} // IPv6 only
+				if !addrIP.IsLinkLocalUnicast() {
+					continue
+				}
+				anAddr.IP = addrIP
+				anAddr.Zone = iface.Name
+				destAddr.Zone = iface.Name
+				msg := []byte(anAddr.String())
+				m.sock.WriteTo(msg, nil, destAddr)
+				break
+			}
+			time.Sleep(time.Second)
+		}
+		time.Sleep(time.Second)
+	}*/
+}
+
+func (m *multicast) listenMulticastDNS() {
+	groupAddr, err := net.ResolveUDPAddr("udp6", m.dnsGroupAddr)
+	if err != nil {
+		panic(err)
+	}
+	bs := make([]byte, 2048)
+	for {
+		nBytes, rcm, fromAddr, err := m.dnsSock.ReadFrom(bs)
+		if nBytes == 0 {
+			continue
+		}
+		if err != nil {
+			panic(err)
+		}
+		if rcm != nil {
+			// Windows can't set the flag needed to return a non-nil value here
+			// So only make these checks if we get something useful back
+			// TODO? Skip them always, I'm not sure if they're really needed...
+			if !rcm.Dst.IsLinkLocalMulticast() {
+				continue
+			}
+			if !rcm.Dst.Equal(groupAddr.IP) {
+				continue
+			}
+			foundintf := false
+			m.zonesMutex.RLock()
+			for _, zone := range m.zones {
+				if zone == fromAddr.(*net.UDPAddr).Zone {
+					foundintf = true
+					break
+				}
+			}
+			m.zonesMutex.RUnlock()
+			if !foundintf {
+				continue
+			}
+		}
+		var msg dnsmessage.Message
+		if err := msg.Unpack(bs); err != nil {
+			continue
+		}
+		m.core.log.Println(fromAddr, msg)
+	}
+}
+
+func (m *multicast) startLegacy() error {
+	if len(m.interfaces()) == 0 {
+		m.core.log.Infoln("Legacy multicast discovery is disabled")
+	} else {
+		m.core.log.Infoln("Legacy multicast discovery is enabled")
 		addr, err := net.ResolveUDPAddr("udp", m.groupAddr)
 		if err != nil {
 			return err
@@ -62,51 +210,13 @@ func (m *multicast) start() error {
 		}
 
 		m.multicastWake()
-		go m.listen()
-		go m.announce()
+		go m.listenLegacy()
+		go m.announceLegacy()
 	}
 	return nil
 }
 
-func (m *multicast) interfaces() []net.Interface {
-	// Get interface expressions from config
-	m.core.configMutex.RLock()
-	exprs := m.core.config.MulticastInterfaces
-	m.core.configMutex.RUnlock()
-	// Ask the system for network interfaces
-	var interfaces []net.Interface
-	allifaces, err := net.Interfaces()
-	if err != nil {
-		panic(err)
-	}
-	// Work out which interfaces to announce on
-	for _, iface := range allifaces {
-		if iface.Flags&net.FlagUp == 0 {
-			// Ignore interfaces that are down
-			continue
-		}
-		if iface.Flags&net.FlagMulticast == 0 {
-			// Ignore non-multicast interfaces
-			continue
-		}
-		if iface.Flags&net.FlagPointToPoint != 0 {
-			// Ignore point-to-point interfaces
-			continue
-		}
-		for _, expr := range exprs {
-			e, err := regexp.Compile(expr)
-			if err != nil {
-				panic(err)
-			}
-			if e.MatchString(iface.Name) {
-				interfaces = append(interfaces, iface)
-			}
-		}
-	}
-	return interfaces
-}
-
-func (m *multicast) announce() {
+func (m *multicast) announceLegacy() {
 	var anAddr net.TCPAddr
 	m.myAddrMutex.Lock()
 	m.myAddr = m.core.tcp.getAddr()
@@ -150,7 +260,7 @@ func (m *multicast) announce() {
 	}
 }
 
-func (m *multicast) listen() {
+func (m *multicast) listenLegacy() {
 	groupAddr, err := net.ResolveUDPAddr("udp6", m.groupAddr)
 	if err != nil {
 		panic(err)
@@ -171,6 +281,18 @@ func (m *multicast) listen() {
 			if !rcm.Dst.Equal(groupAddr.IP) {
 				continue
 			}
+			foundintf := false
+			m.zonesMutex.RLock()
+			for _, zone := range m.zones {
+				if zone == fromAddr.(*net.UDPAddr).Zone {
+					foundintf = true
+					break
+				}
+			}
+			m.zonesMutex.RUnlock()
+			if !foundintf {
+				continue
+			}
 		}
 		anAddr := string(bs[:nBytes])
 		addr, err := net.ResolveTCPAddr("tcp6", anAddr)
@@ -185,4 +307,42 @@ func (m *multicast) listen() {
 		saddr := addr.String()
 		m.core.tcp.connect(saddr, addr.Zone)
 	}
+}
+
+func (m *multicast) interfaces() []net.Interface {
+	// Get interface expressions from config
+	m.core.configMutex.RLock()
+	exprs := m.core.config.MulticastInterfaces
+	m.core.configMutex.RUnlock()
+	// Ask the system for network interfaces
+	var interfaces []net.Interface
+	allifaces, err := net.Interfaces()
+	if err != nil {
+		panic(err)
+	}
+	// Work out which interfaces to announce on
+	for _, iface := range allifaces {
+		if iface.Flags&net.FlagUp == 0 {
+			// Ignore interfaces that are down
+			continue
+		}
+		if iface.Flags&net.FlagMulticast == 0 {
+			// Ignore non-multicast interfaces
+			continue
+		}
+		if iface.Flags&net.FlagPointToPoint != 0 {
+			// Ignore point-to-point interfaces
+			continue
+		}
+		for _, expr := range exprs {
+			e, err := regexp.Compile(expr)
+			if err != nil {
+				panic(err)
+			}
+			if e.MatchString(iface.Name) {
+				interfaces = append(interfaces, iface)
+			}
+		}
+	}
+	return interfaces
 }
